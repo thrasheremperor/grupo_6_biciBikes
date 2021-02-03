dd0bbe70f69dbcac3a7a24aaddd80eb664b47e4a
Maria5398 <mariaisabelbaezrodriguez@gmail.com>
2021-02-03 14:41:28 -0300

    agregando

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+
+/* ROUTER */
+
+const homeRouter = require('./routes/homeRouter');
+const detalleRouter = require('./routes/detalleRouter');
+
 /* VIEW ENGINE */
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/public'));
 
-/* ROUTER */
-
-const homeRouter = require('./routes/homeRouter');
+// rutas 
 
 app.use('/', homeRouter);
+app.use('/detalle', detalleRouter);
 
 app.listen(port, () => console.log(`Server running in port ${port}`))
