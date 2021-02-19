7cd136a1af3d8e6818bcd0507cc04029377e68b4
EzequielArias12 <Ezequiel_arT12@outlook.com>
2021-02-19 02:12:47 -0200

    details

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,7 @@
 module.exports = {
     admin :(req, res) => {
         res.render('formCarga', {
-            title: 'carga de producto'
+            title: 'carrito'
         }) /*renderiso  la vista de admin.ejs */
     }
 }
