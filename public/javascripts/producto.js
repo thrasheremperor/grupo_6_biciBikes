afb1e74ca1725758560a7301a7b5eb58eda0f56f
EzequielArias12 <Ezequiel_arT12@outlook.com>
2021-03-30 11:12:33 -0300

    it

--- public/javascripts/producto.js
+++ public/javascripts/producto.js
@@ -16,6 +16,32 @@ let qs = function (element) {
           $formFileLgError = qs('#formFileLgError'),
           regExAlpha = /^[a-zA-Z\sñáéíóúü ]+$/i,
           regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
-          regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,42}$/;
+          regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,42}$/
     
-  })
+
+         let errores = {};
+
+         $name.addEventListener('blur', function(){
+            switch (true) {
+                case !$name.value.trim():
+                    $nameError.innerHTML = "Debe tener un nombre"
+                    this.classList.add('is-invalid')
+                    
+                    break;
+                case!regExAlpha.test($name.value):
+                     $nameError.innerHTML = 'Debe ingresar un nombre valido'
+                     $name.classList.add('is-invalid') 
+                
+                break;
+            
+                default:
+                   $name.classList.remove('is-invalid')
+                   $name.classList.add('is-valid')
+                   $nameError.innerHTML = null
+                   
+                 break;
+            }
+
+         })
+         
+})
